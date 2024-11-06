7eb56682b873b2dfc676af0da5519b1c243fb75d
David Miksovsky <david.miksovsky@gpjp.cz>
2024-11-06 14:59:23 +0100

    hodina 6.7. 2024

---
+++ 1-1/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys
+
+def cmdline_args():
+    p = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    p.add_argument('required_positional_argument', help='desc')
+    p.add_argument('--on', action='store_true', help='include to enable')
+    p.add_argument('-v', '--verbosity', type= int, choices=(0,1,2), default=0, help='volba vymluvnosti')
+    p.add_argument('-n','--number',type=int, help='Zadej cislo')
+
+    return p.parse_args()
+
+if __name__== '__main__':
+    try:
+        args = cmdline_args()
+        print(args)
+        print(args.required_positional_argument)
+        print(args.verbosity)
+    except:
+        print('try python <script_name> "hello" --on -v 0')
